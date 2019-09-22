a9056063e3baf5c271704bb67d92f3fb0834bd4c
arimura <33759663+arimura@users.noreply.github.com>
2019-09-22 16:58:58 +0900

    init

---
+++ practice-contest/a-welcome-to-atcoder.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c int
+	var d string
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	fmt.Scan(&c)
+	fmt.Scan(&d)
+
+	fmt.Println(a + b + c)
+	fmt.Println(d)
+}
