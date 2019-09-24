27f81fce18e08cc837322b0a649f0b989c619130
arimura <33759663+arimura@users.noreply.github.com>
2019-09-25 07:42:00 +0900

    Add stub

--- practice-contest/interactivesorting/stub/main.go
+++ practice-contest/interactivesorting/stub/main.go
@@ -1,23 +1,54 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"time"
 )
 
 func main() {
-	//read query
+	fmt.Println("stub start")
+
 	var fp *os.File
 	var err error
-	fp, err = os.Open("query.txt")
+
+	fmt.Println("read ./query.fifo")
+	fp, err = os.OpenFile("./query.fifo", os.O_RDONLY, os.ModeNamedPipe)
 	if err != nil {
 		panic(err)
 	}
-
 	defer fp.Close()
 
-	scanner := bufio.NewScanner(fp)
-	scanner.Scan()
-	fmt.Println(scanner.Text())
+	t := makeTable()
+
+	b := make([]byte, 1024)
+	for {
+		n, err := fp.Read(b)
+		if err != io.EOF && err != nil {
+			panic(err)
+		}
+		if n != 0 {
+			fmt.Printf("query: %s", string(b))
+			lw := t[b[2]]
+			rw := t[b[4]]
+			var a string
+			if lw < rw {
+				a = "<"
+			} else {
+				a = ">"
+			}
+			fmt.Println(a)
+		} else {
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
+
+func makeTable() map[byte]int {
+	return map[byte]int{
+		[]byte("A")[0]: 1,
+		[]byte("B")[0]: 0,
+		[]byte("C")[0]: 2,
+	}
 }
